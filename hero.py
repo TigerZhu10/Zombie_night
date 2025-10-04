9f44068714e5e1ce1f336783f5dc819eacc923e8
Tiger Zhu <143968664+TigerZhu10@users.noreply.github.com>
2025-10-03 20:32:42 -0700

    Jump animate finished(V1.7)

--- hero.py
+++ hero.py
@@ -7,9 +7,26 @@ class Hero():
         self.screen = screen
         self.game_settings = game_settings
         self.tile_map = tile_map
-        self.image = pygame.image.load("./assets/images/player/jump/idle/idle (1).png")
-        self.image = pygame.transform.scale(pygame.image.load("./assets/images/player/jump/idle/idle (1).png"), (58,70))
+
+        # self.move_right_sprites = self.load_images("./animation/boys", "Run", 8)
+
+
+        self.idle_right_sprites = self.load_images("./assets/images/player/jump/idle/", "idle",  9)
+        self.idle_left_sprites = [pygame.transform.flip(s, True, False) for s in self.idle_right_sprites]
+
+        self.moving_right_sprites = self.load_images("./assets/images/player/run/", "Run", 9)
+        self.moving_left_sprites = [pygame.transform.flip(s, True, False) for s in self.moving_right_sprites]
+
+        self.jump_right_sprites = self.load_images("./assets/images/player/jump/", "Jump", 9)
+        self.jump_left_sprites = [pygame.transform.flip(s, True, False) for s in self.jump_right_sprites]
+        
+        
+        # starting image
+        self.image = self.idle_right_sprites[0]
         self.rect = self.image.get_rect()
+        self.facing_right = True
+        self.current_sprite = 0
+
         self.x = random.randint(440,770)
         self.y = random.randint(310, 450)
 
@@ -24,7 +41,23 @@ class Hero():
         self.HORIZONTAL_ACCELERATION = 1
         self.HORIZONTAL_FRICTION = 0.15
         self.VERTICAL_JUMP_SPEED = 10
-        self.GRAVITY = 0.25
+        self.GRAVITY = 0.35
+
+        self.on_ground = False
+
+    def load_images(self, folder, prefix, count):
+        """
+        批量加载并缩放动画帧
+        folder: 子文件夹名 (如 "boy")
+        prefix: 文件名前缀 (如 "Run")
+        count: 帧数量
+        """
+        return [
+            pygame.transform.scale(
+                pygame.image.load(f"{folder}/{prefix} ({i}).png"), (64, 64)
+            ) 
+            for i in range(1, count + 1)# count is 8 so the range is from 1 to (8+1) and you will get a value from 1 to 8
+        ]
 
     def moving_hero(self):
         self.acceleration = vector(0, self.GRAVITY)
@@ -33,13 +66,33 @@ class Hero():
         # When keyup the if statement is false, so acceleration is always zero 
         if self.moving_right and self.rect.right <= self.game_settings.WINDOW_WIDTH:
             self.acceleration.x = self.HORIZONTAL_ACCELERATION
-        if self.moving_left and self.rect.left >= 0:
+            self.facing_right = True
+            if self.on_ground == False:
+                self.animate(self.jump_right_sprites, 0.2)
+            else: 
+                self.animate(self.moving_right_sprites, 0.3)
+
+        elif self.moving_left and self.rect.left >= 0:
             self.acceleration.x = -self.HORIZONTAL_ACCELERATION
+            self.facing_right = False
+            if self.on_ground == False:
+                self.animate(self.jump_left_sprites, 0.2)
+            else:
+                self.animate(self.moving_left_sprites, 0.3)
+                
+        elif self.facing_right:
+            self.animate(self.idle_right_sprites, 0.2)
+            if self.on_ground == False:
+                self.animate(self.jump_right_sprites, 0.2)
+        else:
+            self.animate(self.idle_left_sprites, 0.2)
+            if self.on_ground == False:
+                self.animate(self.jump_left_sprites, 0.2)
 
         # You get a nagative number for acceleration when it's zero
         self.acceleration.x = self.acceleration.x - self.velocity.x * self.HORIZONTAL_FRICTION
 
-        #Don't need to know now, it's about physics 11
+        # Don't need to know now, it's about physics 11
         # The velocity decrease when velocity adds the acceleration which is a nagtive number
         self.velocity = self.velocity + self.acceleration
         self.position = self.position + self.velocity + 0.5 * self.acceleration
@@ -47,7 +100,6 @@ class Hero():
         self.rect.topleft = self.position
     
     def hit_floor(self):
-        self.on_ground = False  
 
         for tile in self.tile_map.all_tiles_rect:
             if self.rect.colliderect(tile):
@@ -57,8 +109,19 @@ class Hero():
                     self.velocity.y = 0
                     self.on_ground = True
 
+    def animate(self, sprite_list, speed):
+        """
+        播放指定动画帧列表
+        sprite_list: 图像帧序列
+        speed: 帧切换速度（建议 0.1 ~ 0.3)
+        """
+        self.current_sprite = (self.current_sprite + speed) % len(sprite_list)
+        self.image = sprite_list[int(self.current_sprite)]
+
     def jump(self):
         if self.on_ground and self.moving_up:
+            # if self.facing_right:
+            #     self.animate(self.jump_right_sprites, 0.2)
             self.velocity.y = -self.VERTICAL_JUMP_SPEED
             self.on_ground = False
 
